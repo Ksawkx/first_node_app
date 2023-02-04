d7144bc000dbb20d1ff290bf251c01d13b12d00d
Ksawkx <ksawk@outlook.com>
2023-02-04 15:11:09 +0100

    fisrt time ?

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const port = 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('I am alive')
+})
+app.get('/about', (req, res) => {
+    res.send('Yes, I am still alive')
+})
+
+app.listen(port)
